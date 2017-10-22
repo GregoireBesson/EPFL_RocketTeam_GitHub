f693227980e290f1fd18181c106b8532a583196c
GregoireBesson <gregoire.besson33@gmail.com>
2017-10-22 10:39:50 +0200

    modify the location of simulation files

--- Matlab/Data/ROROsimTethys/init_rocket.m
+++
@@ -1,8 +0,0 @@
-function prop = init_rocket()
-
-    prop = readtable('rocket_proptethys.txt','Format','%s%f');
-%     motorname=char(table2array(prop(end,1)));
-%     motordata = rocketmotor(motorname);
-
-end
-
