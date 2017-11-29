6e4d0b58d8f1a6365f8f1f827377828874c4dccf
GregoireBesson <gregoire.besson@epfl.ch>
2017-11-29 11:36:10 +0100

    covariances are now based on real moice variances

---
+++ Matlab/Data/DataAirbrakes/kalmanKaltbrunn/kalmanKaltbrunn/motion_model.m
@@ -0,0 +1,27 @@
+function [x_hat] = motion_model(obj, Cd, dt)
+    % motion model based on motion equation
+    % inputs:   obj, class containing previous states
+    %           dt, time increment between two update steps
+    % output:   x_hat, state vector containing the estimated current states
+    
+    x_hat = zeros(3,1);
+    
+    % expected acceleration
+    dm_dt = (obj.mass(obj.i+1)-obj.mass(obj.i))/dt
+    
+    % TODO mettre un truc gloal pour otures ces valeurs
+    rho = 1.225;
+    Aref = 0.0082;
+
+    % et la dérivée de la masse est multipliée par la vitesse
+
+    acc = (obj.thrust(obj.i) - Cd * .5 * rho * Aref * obj.x(2)^2 - dm_dt*obj.x(2))/(obj.mass(obj.i));
+
+%     x_hat(1) = obj.x(1) + obj.x(2)*dt + obj.x(3)*dt^2;
+%     x_hat(2) = obj.x(2) + obj.x(3)*dt;
+%     x_hat(3) = obj.x(3);
+    x_hat(1) = obj.x(1) + obj.x(2)*dt + acc*dt^2;
+    x_hat(2) = obj.x(2) + acc*dt;
+    x_hat(3) = acc;
+    
+end
